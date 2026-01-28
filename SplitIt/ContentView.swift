24ff5f90e2d9cf25c5b55ae2b58070880daca49f
JayTurn93 <jyturner93@gmail.com>
2026-01-27 21:01:33 -0800

    just some new subjects we learned. form, navigation bar, modifying program state, binding state to UI controls, views in loops

--- SplitIt/ContentView.swift
+++ SplitIt/ContentView.swift
@@ -8,16 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var buttonTaps = 0
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationStack {
+                Form {
+                    Section {
+                        Text("Hello World")
+                        Text("Hello World")
+                    }
+                    
+                    Button("Button Taps: \(buttonTaps)") {
+                        self.buttonTaps += 1
+                    }
+                    Section{
+                        ForEach (0..<5) {
+                            Text("Row \($0)")
+                        }
+                      }
+                    Section {
+                        Picker("Select you student: ", selection: $selectedStudent){
+                            ForEach (students, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                    }
+                    }
+                }
+                .navigationTitle(Text("Start"))
+            }
+            .padding()
         }
-        .padding()
+        
     }
-}
+
 
 #Preview {
     ContentView()
