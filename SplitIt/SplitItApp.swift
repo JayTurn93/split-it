b06bf27c64d4851ec6eb1afecef0d42150d586c2
JayTurn93 <jyturner93@gmail.com>
2026-01-27 19:48:17 -0800

    first commit

---
+++ SplitIt/SplitItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitItApp.swift
+//  SplitIt
+//
+//  Created by Jalysa Turner on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct SplitItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
