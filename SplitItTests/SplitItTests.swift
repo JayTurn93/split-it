3a89a2586fa24ec0f01fc81482e24bf9d6eeeb13
JayTurn93 <jyturner93@gmail.com>
2026-01-28 23:14:29 -0800

    new branch for some additions

---
+++ SplitItTests/SplitItTests.swift
@@ -0,0 +1,16 @@
+//
+//  SplitItTests.swift
+//  SplitItTests
+//
+//  Created by Jalysa Turner on 1/28/26.
+//
+
+import Testing
+
+struct SplitItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
